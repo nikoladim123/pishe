b9779b5c2065e603aaecef0b0a60a72a591b02f0
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-24 02:46:37 +0100

    testing country API

--- my-app/src/components/Location.js
+++ my-app/src/components/Location.js
@@ -1,8 +1,21 @@
 import React,{Component} from 'react';
+import axios from 'axios';
 
 class Location extends Component{
-  state ={}
+  state ={
+    country:[]
+  }
+  componentDidMount(){
+    axios.get('https://node-testin.herokuapp.com/api')
+    .then(res=>{
+      this.setState({
+        country:res.data
+      })
+    })
+  }
   render(){
+    var country = this.state.country;
+    console.log(country);
     return(
         <form className="LoactionBox">
           <select>
